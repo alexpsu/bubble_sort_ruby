73ed9eb373b0ff777d01efb8bd7b9d33b2e4587f
Alex Rao <YOUR_GITHUB_EMAIL_ADDRESS>
2016-04-14 11:14:46 -0700

    Finished bubble sort

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,3 +1,19 @@
 def bubble_sort(arr)
-  # code in here
+	return arr if arr.length <= 1
+	i = 0
+	j = 1
+	z = 0
+	while z < arr.length
+		while i < arr.length && j < arr.length
+			if arr[i] > arr[j]
+				arr[i],arr[j] = arr[j],arr[i]
+			end
+			i += 1
+			j += 1
+		end
+		z += 1
+		i = 0
+		j = 1
+	end
+	p arr
 end
